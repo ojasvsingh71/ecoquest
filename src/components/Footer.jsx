c4fbfb54d2731a70291a0da95776b16398b368e6
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-05-15 12:02:56 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2024 EcoQuest. All Rights Reserved.</p>
+      <p>© 2025 EcoQuest. All Rights Reserved.</p>
     </footer>
   );
 }
